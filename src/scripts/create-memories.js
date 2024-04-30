5ddaaa9d0c27bc78ac3c479239e2fff9342ca9e4
Nik <77840519+MasterOfAny@users.noreply.github.com>
2024-04-30 19:15:04 +0300

    Finished logic and storing

--- src/scripts/create-memories.js
+++ src/scripts/create-memories.js
@@ -163,7 +163,7 @@ const memoriesData = [
     },
 ];
 
-export const createMemories = (el) => {
+export const createMemories = (el, lsData) => {
     const $memories = el;
     const autoStickers = ['st_0', 'st_1', 'st_2', 'st_3', 'st_4'];
     for (let [index, memory] of memoriesData.entries()) {
@@ -171,18 +171,18 @@ export const createMemories = (el) => {
         $memory.classList.add('diary__memory');
         const memoryContent = `
             <h4>${memory.name[0] !== 'M' ? `MEMORY ${memory.name}` : `${memory.name} OPTIONAL MEMORY`}</h4>
-            <div class="diary__memory-xp">XP:<input name="m_${memory.name}_xp" type="number"/></div>
+            <div class="diary__memory-xp">XP:<input name="m_${memory.name}_xp" value="${lsData.memories[memory.name]?.xp || ''}" type="number"/></div>
             <div class="diary__memory-attempts">
                 Attempts:
                     <div class="diary__memory-attempt">
-                        <input type="checkbox" id="m_${memory.name}_1" name="1" />
+                        <input type="checkbox" id="m_${memory.name}_1" ${lsData.memories[memory.name]?.attempts["1"] && 'checked'} name="1" />
                         <label for="m_${memory.name}_1">
                             <i></i>
                             <span>1</span>   
                         </label>
                     </div>
                     <div class="diary__memory-attempt">
-                        <input type="checkbox" id="m_${memory.name}_2" name="2" />
+                        <input type="checkbox" id="m_${memory.name}_2" ${lsData.memories[memory.name]?.attempts["2"] && 'checked'} name="2" />
                         <label for="m_${memory.name}_2">
                             <i></i>
                             <span>2</span>
@@ -191,16 +191,16 @@ export const createMemories = (el) => {
             </div>
             ${index > 3 ?
                 `<div class="diary__memory-contract">
-            Contract <input name="c_${memory.name}" type="number" /> completed:
+            Contract <input name="c_${memory.name}" value="${lsData.memories[memory.name]?.contract?.id || ''}" type="number" /> completed:
             <div class="diary__memory-cmark">
-                <input type="checkbox" id="m_${memory.name}_yes" name="yes" />
+                <input type="checkbox" id="m_${memory.name}_yes" ${lsData.memories[memory.name]?.contract["yes"] && 'checked'}  name="yes" />
                 <label for="m_${memory.name}_yes">
                     <i></i>
                     <span>yes</span>
                 </label>
             </div>
             <div class="diary__memory-cmark">
-                <input type="checkbox" id="m_${memory.name}_no" name="no" />
+                <input type="checkbox" id="m_${memory.name}_no"  ${lsData.memories[memory.name]?.contract["no"] && 'checked'} name="no" />
                 <label for="m_${memory.name}_no">
                     <i></i>
                     <span>no</span>
@@ -210,7 +210,7 @@ export const createMemories = (el) => {
             }
         <div class="diary__memory-stickers">
             ${memory?.stickers?.length > 0 ? memory?.stickers?.map(sticker => (
-                `<div class="sticker ${autoStickers.includes(sticker) ? 'auto-sticker' : ''}" data-sticker="${memory.name}:${sticker}">
+                `<div class="sticker ${autoStickers.includes(sticker) ? 'auto-sticker' : ''} ${lsData.memories[memory.name]?.stickers?.includes(sticker) ? 'earned-sticker' : ''}" data-sticker="${memory.name}:${sticker}">
                     <svg width="40" height="40">
                         <use xlink:href="/assets/stickers.svg#${sticker}"></use>
                     </svg>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,78 +2,136 @@ import { createXPTable } from "./create-xp-table";
 import { createMemories } from "./create-memories"
 import { createHQ } from "./hq";
 
+let lsData = {
+    totalXp: 0,
+    memories: {}
+};
+
 const $xp = document.querySelector('.diary__xp');
 const $memories = document.querySelector('.diary__memories');
 const $hq = document.querySelector('.diary__hq');
 
+if (window.localStorage.getItem('acbov_data'))
+    lsData = JSON.parse(window.localStorage.getItem('acbov_data'));
+else
+    window.localStorage.setItem('acbov_data', JSON.stringify(lsData));
+
 createXPTable($xp);
-createMemories($memories);
-createHQ($hq);
+createMemories($memories, lsData);
+createHQ($hq, lsData);
 
 const $stickers = document.querySelectorAll('.sticker:not(.auto-sticker)');
 
-let lsData /* = {
-    totalXp: 0,
-    memories:[{
-        name: '',
-        xp:0,
-        stickers:[],
-        attempts:[],
-        contract:{}
-    }]
-}; */
-if (window.localStorage.getItem('acbov_data')) {
-    lsData = JSON.parse(window.localStorage.getItem('acbov_data'));
-} else {
-    window.localStorage.setItem('acbov_data', JSON.stringify({}))
+if (lsData.totalXp) {
+    const totalXp = $xp.querySelector(`#xp${lsData.totalXp}`);
+    totalXp.classList.add('current-xp');
 }
 
-/**
- * totalXP: 0
- * memory:{
- *  xp: 0
- *  stickers:[];
- *  attempts: [];
- *  contract:
- * }
- */
-
-const saveDataToLs = () => {
-
+const saveDataToLs = (type, data) => {
+    let choosedMemory = lsData.memories[data.memory];
+    if (!choosedMemory) {
+        lsData.memories[data.memory] = {
+            xp: 0,
+            stickers: [],
+            attempts: { "1": false, "2": false },
+            contract: { id: '', yes: false, no: false }
+        };
+        choosedMemory = lsData.memories[data.memory];
+    }
+    switch (type) {
+        case 'memory_xp':
+            choosedMemory.xp = data.xp;
+            break;
+        case 'attempts':
+            choosedMemory.attempts[data.attempt.key] = data.attempt.value;
+            break;
+        case 'contractId':
+            choosedMemory.contract.id = data.id;
+            break;
+        case 'contract':
+            choosedMemory.contract[data.completed.key] = data.completed.value;
+            break;
+        case 'sticker':
+            const stickerExist = choosedMemory.stickers.indexOf(data.sticker);
+            if (stickerExist > -1)
+                choosedMemory.stickers.splice(stickerExist, 1);
+            else
+                choosedMemory.stickers.push(data.sticker);
+            break;
+        case 'totalXP':
+            lsData.totalXp = data;
+            break;
+        default:
+            break;
+    }
+    window.localStorage.setItem('acbov_data', JSON.stringify(lsData));
 }
 
-let xp = 0;
-const storeXpInputValue = (e) => {
+let xp = lsData.totalXp;
+
+const setPrevInputValue = (e) => {
+    e.target.dataset.prevValue = e.target.value || 0;
+}
+
+const storeInputValue = (e) => {
+    if (e.target.name.includes('c')) return;
     const inputValue = e.target.value || 0;
     let previousValue = e.target.getAttribute("data-prev-value") || 0;
-    xp += inputValue - previousValue;
+    if (previousValue !== inputValue) xp += inputValue - previousValue;
     e.target.dataset.prevValue = inputValue;
 }
 
 const showXpOnTable = (e) => {
-    const prevXp = $xp.querySelector('.current-xp');
-    if (prevXp) prevXp.classList.remove('current-xp');
-    const targetXp = $xp.querySelector(`#xp${xp}`);
-    if (targetXp) targetXp.classList.add('current-xp');
+    if (e.target.name.includes('c')) {
+        saveDataToLs('contractId', { "memory": e.target.name.split("_")[1], "id": e.target.value || 0 });
+    }
+    else {
+        saveDataToLs('memory_xp', { "memory": e.target.name.split("_")[1], "xp": e.target.value || 0 });
+        const prevXp = $xp.querySelector('.current-xp');
+        if (prevXp) prevXp.classList.remove('current-xp');
+        const targetXp = $xp.querySelector(`#xp${xp}`);
+        if (targetXp) {
+            targetXp.classList.add('current-xp');
+            saveDataToLs('totalXP', Number(targetXp.innerHTML));
+        }
+    }
 }
+
+const processCheckboxes = (e) => {
+    const inputId = e.target.id.split("_");
+    if (inputId[2].includes('yes') || inputId[2].includes('no'))
+        /** contract */
+        saveDataToLs('contract', { "memory": inputId[1], "completed": { key: inputId[2], value: e.target.checked } });
+    else
+        /** attempts */
+        saveDataToLs('attempts', { "memory": inputId[1], "attempt": { key: e.target.name, value: e.target.checked } });
+}
+
 const delegate = (selector) => (cb) => (e) => e.target.matches(selector) && cb(e);
-const inputDelegate = delegate('.diary__memory-xp input');
+const inputDelegate = delegate('.diary__memory input[type=number]');
+const checkboxDelegate = delegate('.diary__memory input[type=checkbox]');
+
+const setAutoSticker = (memorySequence) => {
+    const split = memorySequence.split('_');
+    if (!split[1].includes('m')) {
+        const targets = [...document.querySelectorAll(`[data-sticker*=${split[0]}_${split[1]}]`)];
+        const earnedStickers = targets.filter(target => target.classList.contains('earned-sticker') && !target.classList.contains('auto-sticker'));
+        const autoSticker = targets.find(target => target.classList.contains('auto-sticker'));
+        if (earnedStickers.length === targets.length - 1) autoSticker?.classList.add('earned-sticker');
+        else autoSticker?.classList.remove('earned-sticker');
+    }
+}
 
 const changeStickerState = (e) => {
     e.currentTarget.classList.toggle('earned-sticker');
-    //if (!lsData?.memory[])
     const data = e.currentTarget.getAttribute("data-sticker").split(':');
-    console.log(data);
-    /* if (!lsData?.memory[data[0]]) {
-
-    } */
-    lsData.memories.push({
-        name: data[0],
-        //  stickers
-    })
+    setAutoSticker(data[1]);
+    saveDataToLs('sticker', { "memory": data[0], "sticker": data[1] });
 }
 
-$memories.addEventListener('input', inputDelegate((el) => storeXpInputValue(el)));
+$memories.addEventListener('click', checkboxDelegate((el) => processCheckboxes(el)));
+$memories.addEventListener('input', inputDelegate((el) => storeInputValue(el)));
+$memories.addEventListener('focusin', inputDelegate((el) => setPrevInputValue(el)));
 $memories.addEventListener('focusout', inputDelegate((el) => showXpOnTable(el)));
 $stickers?.forEach((sticker => sticker.addEventListener('click', (e) => changeStickerState(e))));
 
