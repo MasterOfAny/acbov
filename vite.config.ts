0cc106735e4bb53f08a449ecd54edf20a45119b7
Nik <77840519+MasterOfAny@users.noreply.github.com>
2024-11-15 17:37:23 +0300

    fix configs for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/public/*',
+                    dest: ''
+                }
+            ]
+        })
+    ],
     build: {
         target: 'esnext',
         rollupOptions: {
-            input: {
-                app: './src/index.html', // default
+            input: './src/index.html',
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]',
+                dir: 'dist',
             },
         },
     },
